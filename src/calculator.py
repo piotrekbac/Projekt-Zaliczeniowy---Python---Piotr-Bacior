ccb0491dadc135a9621d5b682a50bbf83f7afd9e
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-02 17:03:38 +0200

    Dodanie docstringa z podstawowymi informacjami o funkcji

--- src/calculator.py
+++ src/calculator.py
@@ -1,2 +1,11 @@
 # Definiuję funkcję do obliczania BMI (Body Mass Index) - dane wejściowe są typu float, oczekujemy wyniku typu float 
 def calculate_bmi(weight_kg: float, height_m: float) -> float: 
+    
+    """
+    Oblicza BMI na podstawie wagi (w kilogramach) i wzrostu (w metrach).
+    
+    Oczekiwane dane wejściowe:
+    - weight_kg: waga w kilogramach (float)
+    - height_m: wzrost w metrach (float)
+
+    """
