845f24746a7d715365175182ac8994377e2ee574
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-02 17:05:40 +0200

    Dodanie do docstringa funkcji calculate_bmi odpowiednich opisu ograniczeń danych wejściowych, które chcemy zaimplementować w samej funkcji

--- src/calculator.py
+++ src/calculator.py
@@ -1,11 +1,18 @@
 # Definiuję funkcję do obliczania BMI (Body Mass Index) - dane wejściowe są typu float, oczekujemy wyniku typu float 
 def calculate_bmi(weight_kg: float, height_m: float) -> float: 
-    
+
     """
-    Oblicza BMI na podstawie wagi (w kilogramach) i wzrostu (w metrach).
+    Oblicza BMI na podstawie wagi (w kilogramach) i wzrostu (w metrach)
     
     Oczekiwane dane wejściowe:
     - weight_kg: waga w kilogramach (float)
     - height_m: wzrost w metrach (float)
+
+    Zachowanie funkcji:
+    - Oblicza stosunek wagi do kwadratu wzrostu (BMI = waga / (wzrost^2))
+    - Zwraca wskaźnik BMI jako wartość zmiennoprzecinkową (float)
+
+    Ograniczenia: 
+    - Oczekiwane wartosci 'weight_kg' i 'height_m' muszą być dodatnie (> 0)
 
     """
