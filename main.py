f025376cd922fca7a5bf44a2c2b30c8ee0b1a80a
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-02 17:27:07 +0200

    Docstring do działania funkcji main() w pliku main.py

--- main.py
+++ main.py
@@ -2,4 +2,19 @@ from src.calculator import calculate_bmi    # Importujemy funkcję calculate_bmi
 
 # Główna funkcja programu, która będzie wywoływana podczas uruchamiania skryptu
 def main():
+    """
+    Punkt wejścia programu Kalkulatora BMI
+
+    Oczekiwane dane wejściowe:
+    - Brak bezpośrednich argumentów funkcji (na razie dane wpisywane będą na sztywno
+      w kodzie - docelowo będziemy je pobierać z klawiatury)
+
+    Zachowanie funkcji:
+    - Wyświetla interfejs powitalny dla użytkownika w konsoli
+    - Wywołuje funkcję obliczającą BMI z podanymi parametrami
+    - Formatuje i wyświetla wynik obliczeń BMI w czytelny sposób na ekranie
+
+    Ograniczenia:
+    - Funkcja przeznaczoa jest do działania w środowisku konsolowym
     
+    """
