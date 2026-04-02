fc4f7b8a771342bbbc02ec7975aa1a1b435e87ab
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-02 17:50:22 +0200

    Definiuję funkcję analyze_bmi - analizuje wskaźnik bmi i zwraca odpowiednią kategorię słowną

---
+++ src/analyzer.py
@@ -0,0 +1,2 @@
+# Definiuję funkcję do analizy wartości BMI i zwracania odpowiedniego komunikatu - dane wejściowe są typu float, oczekujemy wyniku typu str
+def analyze_bmi(bmi_value: float) -> str:
