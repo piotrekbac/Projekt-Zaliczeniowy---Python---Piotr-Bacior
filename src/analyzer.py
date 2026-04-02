652c95ba2e7fa5a82db39e40f961a1db6c0ae2e0
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-02 17:52:26 +0200

    Docstring do funkcji analyze_bmi

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,2 +1,13 @@
 # Definiuję funkcję do analizy wartości BMI i zwracania odpowiedniego komunikatu - dane wejściowe są typu float, oczekujemy wyniku typu str
 def analyze_bmi(bmi_value: float) -> str:
+    """
+    Analizuje wskaźnik BMI i zwraca odpowiednią kategorię zdrowotną na podstawie wartości BMI
+
+    Oczekiwane dane wejściowe:
+    - bmi_value: wartość wskaźnika BMI (float)
+
+    Zachowanie funkcji:
+    - Porównuje wartosci BMI z przyjętymi normami światowej organizacji zdrowia (WHO) 
+    - Zwraca łańcuch znaków (str) z nazwą odpowiedniej kategorii zdrowotnej 
+
+    """
