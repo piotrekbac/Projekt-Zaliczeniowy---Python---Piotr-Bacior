38410ba7abb0e44d5a119bd66f2abd3587bf60e6
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-02 17:24:04 +0200

    Definiowanie głównej funkcji programu - main - punkt wejścia programu Kalkulatora BMI

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 from src.calculator import calculate_bmi    # Importujemy funkcję calculate_bmi z modułu calculator
+
+# Główna funkcja programu, która będzie wywoływana podczas uruchamiania skryptu
+def main():
+    
