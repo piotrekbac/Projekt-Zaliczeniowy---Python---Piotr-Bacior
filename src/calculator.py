e561451f2bcc90d09048cdc61f9a6fd2e25db5cf
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-02 17:02:48 +0200

    Definiowanie funkcji calculate_bmi - inicjalizacja funkcji

---
+++ src/calculator.py
@@ -0,0 +1,2 @@
+# Definiuję funkcję do obliczania BMI (Body Mass Index) - dane wejściowe są typu float, oczekujemy wyniku typu float 
+def calculate_bmi(weight_kg: float, height_m: float) -> float: 
