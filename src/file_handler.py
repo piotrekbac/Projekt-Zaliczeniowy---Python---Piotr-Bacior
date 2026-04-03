fd068d007ef5bca620d2c58f88d19bd09e591d20
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-04 01:40:07 +0200

    Inicjalizacja funkcji save_result_to_file

--- src/file_handler.py
+++ src/file_handler.py
@@ -1,1 +1,3 @@
 from datetime import datetime       # importujemy datetime, aby móc pracować z datami i czasem
+
+def save_result_to_file(weight_kg: float, height_m: float, bmi_value: float, category: str, filename: str):
