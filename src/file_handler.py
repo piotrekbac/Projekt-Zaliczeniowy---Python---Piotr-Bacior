5621e3432ef79f731fc6a508f2fb31ec0361a979
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-04 01:41:59 +0200

    Tworzenie docstringa do funkcji save_result_to_file

--- src/file_handler.py
+++ src/file_handler.py
@@ -2,3 +2,15 @@ from datetime import datetime       # importujemy datetime, aby móc pracować z
 
 # Funkcja save_result_to_file zapisuje wynik obliczeń BMI do pliku tekstowego, wraz z datą i czasem, oraz informacjami o wadze, wzroście, wartości BMI i kategorii zdrowotnej.
 def save_result_to_file(weight_kg: float, height_m: float, bmi_value: float, category: str, filename: str = "bmi_results.txt") -> None:
+    
+    """
+    Zapisuje wynik obliczeń BMI do pliku tekstowego wraz z obecną datą i godziną 
+
+    Oczekiwane dane wejściowe:
+    - weight_kg: waga w kilogramach (float)
+    - height_m: wzrost w metrach (float)
+    - bmi_value: obliczone BMI (float)
+    - category: kategoria zdrowotna (str)
+    - filename: nazwa pliku, do którego zostanie zapisany wynik (domyślnie "bmi_results.txt")
+
+    """
