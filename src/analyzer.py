36c7467bed73cbb9ea82a221458f0d20869378b1
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-04 01:49:42 +0200

    Podpisanie pliku analyzer.py

--- src/analyzer.py
+++ src/analyzer.py
@@ -1,3 +1,5 @@
+# Piotr Bacior - 15 722 - 2026 - Python - MH
+
 # Definiuję funkcję do analizy wartości BMI i zwracania odpowiedniego komunikatu - dane wejściowe są typu float, oczekujemy wyniku typu str
 def analyze_bmi(bmi_value: float) -> str:
 
