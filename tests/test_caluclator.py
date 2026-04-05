2223858a451dd973564acaf54345c1008fc75de1
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-05 18:27:43 +0200

    Podpisanie pliku test_calulator.py

--- tests/test_caluclator.py
+++ tests/test_caluclator.py
@@ -1,2 +1,7 @@
 import unittest                                                     # Importujemy moduł unittest, który służy do tworzenia i uruchamiania testów jednostkowych
 from src.calculator import calculate_bmi, calculate_ideal_weight    # Importujemy funkcje calculate_bmi i calculate_ideal_weight z modułu calculator
+
+# Piotr Bacior - 15 722 - 2026 - Python - MH
+
+# Tworzymy klasę TestCalculator, która dziedziczy po unittest.TestCase, co pozwala nam definiować metody testowe 
+class TestCalculator(unittest.TestCase): 
