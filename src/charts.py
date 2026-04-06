80aeb5e5122b23dc4fa6ab3eda1b7a468d3e7205
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-07 01:38:04 +0200

    Import matplotlib do rysowania wykresów w pliku charts.py

--- src/charts.py
+++ src/charts.py
@@ -1,2 +1,3 @@
 import os                                      # importujemy moduł os, aby móc pracować z plikami i ścieżkami
 import pandas as pd                            # importujemy pandas, aby móc pracować z danymi w formacie DataFrame
+import matplotlib.pyplot as plt                # importujemy matplotlib, aby móc tworzyć wykresy
