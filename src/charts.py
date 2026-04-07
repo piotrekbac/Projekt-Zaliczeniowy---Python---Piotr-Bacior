7a9c516d81b4d947765284433468558206127565
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-08 00:04:13 +0200

    Inicjalizacja funkcji show_bmi_trend_chart

--- src/charts.py
+++ src/charts.py
@@ -3,3 +3,6 @@ import pandas as pd                            # importujemy pandas, aby móc pr
 import matplotlib.pyplot as plt                # importujemy matplotlib, aby móc tworzyć wykresy
 
 # Piotr Bacior - 15 722 - 2026 - Python - MH
+
+# Funkcja show_bmi_trend_chart przyjmuje nazwę pliku CSV jako argument i wyświetla wykres trendu BMI na podstawie danych z tego pliku.
+def show_bmi_trend_chart(csv_filename: str = "historia_bmi.csv") -> None:
