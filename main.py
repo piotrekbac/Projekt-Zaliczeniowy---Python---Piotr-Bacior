f893a18b4095cd15ff19f0ea8b4e6f922a07845d
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-10 00:23:31 +0200

    Wypisywanie info - tryb 4 - bardzo aktywny tryb życia

--- main.py
+++ main.py
@@ -117,6 +117,7 @@ def main():
             print('1. Siedzący tryb życia (brak lub minimalna aktywność fizyczna)')
             print('2. Lekko aktywny (lekkie ćwiczenia/sport 1-3 dni w tygodniu)')
             print('3. Umiarkowanie aktywny (umiarkowane ćwiczenia/sport 3-5 dni w tygodniu)')
+            print('4. Bardzo aktywny (intensywne ćwiczenia/sport 6-7 dni w tygodniu)')
             
 
             # Wywołujemy funkcję calculate_bmi z przykładowymi danymi
