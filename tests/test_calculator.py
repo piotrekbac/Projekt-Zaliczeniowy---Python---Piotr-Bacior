acca056fede2e530e765b38ba08657b76de946cf
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-12 00:43:28 +0200

    Zamiana miejscami testów - dla zachowania estetyki w projekcie

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -89,7 +89,8 @@ class TestCalculator(unittest.TestCase):
             calculate_bmr(90.0, -1.80, 30, 'm')     # Wzrost ujemny - mężczyzna
 
         with self.assertRaises(ValueError):
+            calculate_bmr(90.0, 1.80, -30, 'm')     # Wiek ujemny - mężczyzna
+            
+        with self.assertRaises(ValueError):
             calculate_bmr(70.0, 1.70, -26, 'k')     # Wiek ujemny - kobieta
         
-        with self.assertRaises(ValueError):
-            calculate_bmr(90.0, 1.80, -30, 'm')     # Wiek ujemny - mężczyzna
