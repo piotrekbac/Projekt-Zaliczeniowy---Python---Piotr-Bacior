5027c5b7ef68ba2409f43847a1a90341e95e8da3
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-11 11:55:01 +0200

    Wizualne poprawki w nagłówku ponownego obliczania bmi

--- main.py
+++ main.py
@@ -270,9 +270,9 @@ def main():
 
         # Jeżeli użytkownik wpisał 't' (tak), kontynuujemy działanie programu (wracamy do początku pętli while True)
         elif wybor == 't':     
-            print("\n" + "=" * 30 + "\n")                                       # Dodajemy odstęp i linię oddzielającą wyniki od reszty interfejsu
+            print("\n" + "=" * 70 + "\n")                                       # Dodajemy odstęp i linię oddzielającą wyniki od reszty interfejsu
             print("Nowe obliczenie BMI")                                        # Informujemy użytkownika, że rozpoczynamy nowe obliczenie BMI
-            print("\n" + "=" * 30 + "\n")                                       # Dodajemy odstęp i linię oddzielającą wyniki od reszty interfejsu
+            print("\n" + "=" * 70 + "\n")                                       # Dodajemy odstęp i linię oddzielającą wyniki od reszty interfejsu
 
 
 # Uruchamiamy główną funkcję programu
