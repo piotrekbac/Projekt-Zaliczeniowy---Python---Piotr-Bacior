b0e0c39c7364eaef16f91910251d1390d26211ec
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-23 00:54:30 +0200

    Import odpowiednich funkcji z pliku file_handler

--- tests/test_file_handler.py
+++ tests/test_file_handler.py
@@ -2,3 +2,6 @@ import unittest             # importuję moduł unittest do tworzenia testów je
 import os                   # importuję moduł os do operacji na plikach
 import tempfile             # importuję moduł tempfile do tworzenia tymczasowych plików
 import csv                  # importuję moduł csv do obsługi plików CSV
+
+# Importuję funkcje save_result_to_file, read_history_from_file i save_to_csv z modułu file_handler
+from src.file_handler import save_result_to_file, read_history_from_file, save_to_csv   
