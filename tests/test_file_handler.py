8cd29600683db7251c52fe0a21fab3820e3c166f
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-23 00:54:54 +0200

    Podpisanie pliku test_file_handler.py

--- tests/test_file_handler.py
+++ tests/test_file_handler.py
@@ -5,3 +5,5 @@ import csv                  # importuję moduł csv do obsługi plików CSV
 
 # Importuję funkcje save_result_to_file, read_history_from_file i save_to_csv z modułu file_handler
 from src.file_handler import save_result_to_file, read_history_from_file, save_to_csv   
+
+# Piotr Bacior - 15 722 - 2026 - Python - MH
