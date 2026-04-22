8ffda7acc585623e8fdf5d31e23150018c7dd117
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-23 00:53:38 +0200

    Importowanie modułu tempfile - do tworzenia plików tymczasowych

--- tests/test_file_handler.py
+++ tests/test_file_handler.py
@@ -1,2 +1,3 @@
 import unittest             # importuję moduł unittest do tworzenia testów jednostkowych
 import os                   # importuję moduł os do operacji na plikach
+import tempfile             # importuję moduł tempfile do tworzenia tymczasowych plików
