dcdaca13b9420d9c967553b676e9e82dae12e122
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-23 00:58:58 +0200

    Dokonujemy sprawdzenia poprawności danych

--- tests/test_file_handler.py
+++ tests/test_file_handler.py
@@ -29,3 +29,8 @@ class TestFileHandler(unittest.TestCase) :
 
         # Krok 2: Odczyt
             history = read_history_from_file(filename=test_file)
+
+        # Krok 3: Sprawdzanie
+            self.assertEqual(len(history), 1)                   # Powinna być dokładnie 1 linijka
+            self.assertIn("Waga: 70.0", history[0])             # Sprawdzamy czy nasza waga się zapisała
+            self.assertIn("Wzrost: 1.75", history[0])           # Sprawdzamy czy nasz wzrost się zapisał
