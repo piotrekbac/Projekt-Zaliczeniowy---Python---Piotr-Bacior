a57971b32feef30e4706f93ddcf591a2852d6373
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-23 00:55:57 +0200

    Docstring do klasy TestFileHandler

--- tests/test_file_handler.py
+++ tests/test_file_handler.py
@@ -11,3 +11,5 @@ from src.file_handler import save_result_to_file, read_history_from_file, save_t
 
 # Tworzę klasę TestFileHandler, która dziedziczy po unittest.TestCase, co pozwala mi definiować metody testowe dla funkcji save_result_to_file, read_history_from_file i save_to_csv
 class TestFileHandler(unittest.TestCase) : 
+
+    """ Klasa testująca zapis i odczyt z systemu plików (TXT oraz CSV). """
