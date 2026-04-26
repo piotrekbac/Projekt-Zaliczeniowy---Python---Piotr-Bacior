c69dfcfa46fa597c304e22a70767cbed132edd29
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-27 01:28:10 +0200

    Komentarz do importu modułu FPDF do tworzenia naszych pdf'ów

--- src/pdf_generator.py
+++ src/pdf_generator.py
@@ -1,4 +1,4 @@
-from fpdf import FPDF 
+from fpdf import FPDF          # Importujemy klasę FPDF z biblioteki fpdf, która służy do generowania plików PDF
 
 # Piotr Bacior - 15 722 - 2026 - Python - MH
 
