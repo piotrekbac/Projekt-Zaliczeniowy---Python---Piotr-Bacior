c90914ae1aca1e67cd98fe8e7361735c3bab076b
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-27 01:28:34 +0200

    Import modułu os

--- src/pdf_generator.py
+++ src/pdf_generator.py
@@ -1,4 +1,6 @@
-from fpdf import FPDF          # Importujemy klasę FPDF z biblioteki fpdf, która służy do generowania plików PDF
+from fpdf import FPDF               # Importujemy klasę FPDF z biblioteki fpdf, która służy do generowania plików PDF
+import os                           # Importujemy moduł os, który pozwala na interakcję z systemem plików (np. sprawdzanie istnienia pliku, usuwanie pliku)
+
 
 # Piotr Bacior - 15 722 - 2026 - Python - MH
 
