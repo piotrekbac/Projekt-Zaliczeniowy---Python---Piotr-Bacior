5b0fd7bf274b143de0b055024a26203ddf83365a
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-28 00:59:37 +0200

    Definiowanie słownika akcentów w języku polskim

--- src/pdf_generator.py
+++ src/pdf_generator.py
@@ -8,4 +8,7 @@ from datetime import datetime       # Importujemy klasę datetime z modułu date
 # Definiuję funkcję do generowania pliku PDF z analizą BMI - dane wejściowe są typu str, oczekujemy wyniku typu None (funkcja nie zwraca wartości)
 def remove_polish_accents(text: str) -> str : 
 
-     """ Funkcja pomocnicza usuwająca polskie znaki dla standardowych czcionek PDF. """
+    """ Funkcja pomocnicza usuwająca polskie znaki dla standardowych czcionek PDF. """
+
+    accents = {'ą':'a', 'ć':'c', 'ę':'e', 'ł':'l', 'ń':'n', 'ó':'o', 'ś':'s', 'ź':'z', 'ż':'z',
+            'Ą':'A', 'Ć':'C', 'Ę':'E', 'Ł':'L', 'Ń':'N', 'Ó':'O', 'Ś':'S', 'Ź':'Z', 'Ż':'Z'}
