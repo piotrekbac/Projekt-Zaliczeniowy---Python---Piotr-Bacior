dab48c922c537dc90638503e346c693e3f12235b
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-28 01:00:03 +0200

    Komentarz do świeżo zdefiniowanego łanucha znaków polskich

--- src/pdf_generator.py
+++ src/pdf_generator.py
@@ -10,5 +10,6 @@ def remove_polish_accents(text: str) -> str :
 
     """ Funkcja pomocnicza usuwająca polskie znaki dla standardowych czcionek PDF. """
 
+    # Tworzymy nowy łańcuch znaków, zastępując polskie znaki ich odpowiednikami bez akcentów
     accents = {'ą':'a', 'ć':'c', 'ę':'e', 'ł':'l', 'ń':'n', 'ó':'o', 'ś':'s', 'ź':'z', 'ż':'z',
             'Ą':'A', 'Ć':'C', 'Ę':'E', 'Ł':'L', 'Ń':'N', 'Ó':'O', 'Ś':'S', 'Ź':'Z', 'Ż':'Z'}
