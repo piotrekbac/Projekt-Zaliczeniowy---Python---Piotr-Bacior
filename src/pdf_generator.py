5437432b39156cc3e6ac9bb1bc1c24abdce45a75
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-04-29 00:49:31 +0200

    Dodajemy info o węglowodanach

--- src/pdf_generator.py
+++ src/pdf_generator.py
@@ -85,3 +85,6 @@ def generate_pdf_report(waga: float, wzrost: float, bmi: float, kategoria: str, 
 
     # Dodajemy szczegółowe informacje o zapotrzebowaniu kalorycznym i makroskładnikach, usuwając polskie znaki, aby zapewnić poprawne wyświetlanie w PDF
     pdf.cell(200, 8, txt=remove_polish_accents(f"-> Tluszcze: {tluszcze} g"), ln=True)
+
+    # Dodajemy szczegółowe informacje o zapotrzebowaniu kalorycznym i makroskładnikach, usuwając polskie znaki, aby zapewnić poprawne wyświetlanie w PDF
+    pdf.cell(200, 8, txt=remove_polish_accents(f"-> Weglowodany: {wegle} g"), ln=True)
