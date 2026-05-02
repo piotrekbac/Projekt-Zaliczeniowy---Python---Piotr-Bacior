88c37ccdcc1bf40fe9acf83781ac32f8dcfb8642
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-05-02 18:11:13 +0200

    Poprawki wizualne w pliku prediction.py

--- src/prediction.py
+++ src/prediction.py
@@ -1,4 +1,4 @@
-import os                       # Importujemy moduł os, który pozwala na interakcję z systemem plików, np. do sprawdzania istnienia plików czy tworzenia ścieżek
-import pandas as pd             # Importujemy bibliotekę pandas, która jest używana do manipulacji danymi i tworzenia DataFrame'ów, co ułatwia pracę z danymi tabelarycznymi
-import numpy as np              # Importujemy bibliotekę numpy, która jest używana do operacji na tablicach i macierzach, co jest przydatne do obliczeń numerycznych
-from datetime import timedelta  # Importujemy datetime i timedelta, które są używane do operacji na datach i czasie, np. do obliczania różnic między datami
+import os                               # Importujemy moduł os, który pozwala na interakcję z systemem plików, np. do sprawdzania istnienia plików czy tworzenia ścieżek
+import pandas as pd                     # Importujemy bibliotekę pandas, która jest używana do manipulacji danymi i tworzenia DataFrame'ów, co ułatwia pracę z danymi tabelarycznymi
+import numpy as np                      # Importujemy bibliotekę numpy, która jest używana do operacji na tablicach i macierzach, co jest przydatne do obliczeń numerycznych
+from datetime import timedelta          # Importujemy datetime i timedelta, które są używane do operacji na datach i czasie, np. do obliczania różnic między datami
