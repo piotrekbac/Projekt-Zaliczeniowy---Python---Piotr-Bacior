7f36ec90e78d6b4d997327a7ba5331eb144a9fea
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-05-02 17:34:57 +0200

    Dodanie komnetarza do działania zwracanego komunikatu o braku danych dla stworzenia wykresu

--- src/pdf_generator.py
+++ src/pdf_generator.py
@@ -234,6 +234,7 @@ def generate_pdf_report(waga: float, wzrost: float, bmi: float, kategoria: str, 
     # Jeśli plik z wykresem trendu wagi nie istnieje, dodajemy informację o braku danych do raportu PDF, co pozwala na przekazanie użytkownikowi informacji o braku wykresu trendu wagi w raporcie PDF z analizą BMI i zaleceniami dietetycznymi
     else :
 
+        # Dodajemy informację o braku danych do wygenerowania wykresu trendu wagi, usuwając polskie znaki, aby zapewnić poprawne wyświetlanie w PDF, i ustawiając ją na lewej stronie (align='L'), co informuje użytkownika o braku wykresu trendu wagi w raporcie PDF z analizą BMI i zaleceniami dietetycznymi
         normal_text("Brak danych do wygenerowania wykresu trendu wagi.", bold=True)
 
     # Sekcja Podsumowania - dodajemy podsumowanie i zalecenia końcowe do raportu - zapis
