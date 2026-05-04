06c4c61bcc7580ae0fd3c42ebbe57779c0308b7d
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-05-04 18:21:27 +0200

    Tworzymy odpowiednią ścieżkę do pliku pusty.csv

--- tests/test_analyzer.py
+++ tests/test_analyzer.py
@@ -81,4 +81,6 @@ class TestAnalyzer(unittest.TestCase) :
 
         """ Sprawdza, czy funkcja predykcyjna poprawnie zwraca info o braku danych, gdy nie ma historii """
 
-        with tempfile.TemporaryDirectory() as temp_dir:          # Tworzę tymczasowy katalog, który zostanie automatycznie usunięty po zakończeniu testu
+        with tempfile.TemporaryDirectory() as temp_dir:             # Tworzę tymczasowy katalog, który zostanie automatycznie usunięty po zakończeniu testu
+
+            fake_csv = os.path.join(temp_dir, "pusty.csv")          # Tworzę ścieżkę do tymczasowego pliku CSV
