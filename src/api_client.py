088f0ce7a5a8fd9e176a90688585fd70b28d5336
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-05-07 11:17:16 +0200

    Podpisanie pliku api_client.py

--- src/api_client.py
+++ src/api_client.py
@@ -1,2 +1,4 @@
 import requests                                                 # import modułu requests do obsługi zapytań HTTP
 import random                                                   # import modułu random do generowania losowych danych (symulacja)
+
+# Piotr Bacior - 15 722 - 2026 - Python - MH
