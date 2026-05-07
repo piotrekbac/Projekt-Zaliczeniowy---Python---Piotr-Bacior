dc486a94c9ebca8e8324558fb0c201720a7ac156
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-05-07 11:17:55 +0200

    Informacja w kodzie dla osoby czytającej o działaniu API

--- src/api_client.py
+++ src/api_client.py
@@ -2,3 +2,6 @@ import requests                                                 # import modułu
 import random                                                   # import modułu random do generowania losowych danych (symulacja)
 
 # Piotr Bacior - 15 722 - 2026 - Python - MH
+
+# Tutaj podaję swoje darmowe klucze z serwisu Edamam.com (zakładka Recipe Search API).
+# Jeśli zostawiamy je puste, program automatycznie użyje wbudowanego trybu "Fallback" (Zabezpieczającego).
