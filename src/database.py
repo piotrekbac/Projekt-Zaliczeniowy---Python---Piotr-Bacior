c8917ba012b9e40aa34179e9430e642892c57d6d
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-05-09 01:22:38 +0200

    Podpisanie pliku database.py

--- src/database.py
+++ src/database.py
@@ -1,3 +1,5 @@
 import sqlite3                                          # Import modułu sqlite3 do obsługi bazy danych SQLite
 import pandas as pd                                     # Import modułu pandas do manipulacji danymi
 from datetime import datetime                           # Import modułu datetime do obsługi dat i czasu
+
+# Piotr Bacior - 15 722 - 2026 - Python - MH
