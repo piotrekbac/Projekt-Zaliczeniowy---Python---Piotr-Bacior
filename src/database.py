0316173fb0641dc6f4068382f29568682e6885bd
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-05-09 01:22:07 +0200

    Import modułu datetime do obsługi dat oraz czasu

--- src/database.py
+++ src/database.py
@@ -1,2 +1,3 @@
 import sqlite3                                          # Import modułu sqlite3 do obsługi bazy danych SQLite
 import pandas as pd                                     # Import modułu pandas do manipulacji danymi
+from datetime import datetime                           # Import modułu datetime do obsługi dat i czasu
