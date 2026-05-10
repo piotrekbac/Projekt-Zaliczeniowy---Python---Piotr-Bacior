c489e6b351b2576b0160693a3897a8d4a9832e83
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-05-11 00:42:12 +0200

    Sortujemy dane według daty

--- src/prediction.py
+++ src/prediction.py
@@ -76,3 +76,6 @@ def predict_goal_date(csv_filename: str, target_weight: float) -> str :
         
     # Konwertujemy kolumnę z datami na format datetime, co pozwala nam na łatwe obliczenia związane z czasem
     df[date_col] = pd.to_datetime(df[date_col])  
+
+    # Sortujemy dane według daty, co jest ważne dla analizy trendu    
+    df = df.sort_values(by=date_col)  
