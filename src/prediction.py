8b5027721f42d4908078430145579abf8058477b
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-05-11 00:41:53 +0200

    Konwertujemy kolumnę z datami na odpowiedni format

--- src/prediction.py
+++ src/prediction.py
@@ -73,3 +73,6 @@ def predict_goal_date(csv_filename: str, target_weight: float) -> str :
 
             # Jeśli jest mniej niż 2 wiersze, zwracamy komunikat o braku wystarczających danych historycznych, co oznacza, że nie możemy przewidzieć daty osiągnięcia celu wagowego
             return "Potrzebujesz minimum 2 pomiarów w historii, aby wyliczyć trend"
+        
+    # Konwertujemy kolumnę z datami na format datetime, co pozwala nam na łatwe obliczenia związane z czasem
+    df[date_col] = pd.to_datetime(df[date_col])  
