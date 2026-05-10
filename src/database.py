68d9a5226dc8bc74d72f812617e12f8f49fdcdb2
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-05-10 02:06:00 +0200

    Dodanie odpowiednich danych do zapytania execute od cursora

--- src/database.py
+++ src/database.py
@@ -79,4 +79,4 @@ def init_db() :
                        
             # Sekcja VALUES określa wartości, które mają zostać wstawione do tabeli, odpowiadające kolumnom wymienionym wcześniej. Znaki zapytania (?) są używane jako miejsca na wartości, które zostaną przekazane jako argumenty funkcji.
             VALUES (?, ?, ?, ?, ?, ?, ?)
-        ''')
+        ''', (now, waga, wzrost, bmi, kategoria, min_waga, max_waga))
