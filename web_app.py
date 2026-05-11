294ae626c9f2e0ff4878c1a3d6db201274e62433
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-05-12 00:27:38 +0200

    Import autorskich funkcji z pliku database

--- web_app.py
+++ web_app.py
@@ -8,6 +8,7 @@ from src.calculator import calculate_bmi, calculate_ideal_weight, calculate_bmr,
 from src.analyzer import analyze_bmi, calculate_weight_difference                               
 from src.prediction import predict_goal_date                                       
 from src.api_client import fetch_recipes_from_api, get_meal_suggestions, meal_kcal               
+from src.database import save_measurement_to_csv, load_history_from_csv             
 
 # Piotr Bacior - 15 722 - 2026 - Python - MH
 
