817dcebb5f07755856fb149803f6b0cb4cdcf7f9
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-05-14 00:27:46 +0200

    Podpisanie pliku test_api_client.py

--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -1,2 +1,5 @@
 import unittest                                                 # importuję moduł unittest do tworzenia testów jednostkowych
 from src.api_client import get_meal_suggestions                 # importuję funkcję get_meal_suggestions z modułu api_client
+
+# Piotr Bacior - 15 722 - 2026 - Python - MH
+
