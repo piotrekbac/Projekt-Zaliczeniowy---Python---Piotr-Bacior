c331e3bc352f43ece768c2f4a6cae9af5a7dece3
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-05-13 22:19:06 +0200

    Import funkcji z pliku pdf_generator

--- web_app.py
+++ web_app.py
@@ -10,6 +10,7 @@ from src.prediction import predict_goal_date
 from src.api_client import fetch_recipes_from_api, get_meal_suggestions, meal_kcal               
 from src.database import init_db, save_measurement_to_csv, load_history_from_csv            
 from src.prediction import predict_goal_from_sql 
+from src.pdf_generator import generate_pdf_report
 
 # Piotr Bacior - 15 722 - 2026 - Python - MH
 
