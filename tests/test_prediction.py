bdbd1aa7761700716e5460fe8ab39e6542e1a050
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-05-15 17:30:02 +0200

    Import modułu pandas

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -1,1 +1,2 @@
 import unittest                                            # importuję moduł unittest do tworzenia testów jednostkowych 
+import pandas as pd                                        # importuję moduł pandas do manipulacji danymi w formie DataFrame
