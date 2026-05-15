ec7db381addc51c03b2242ab627da811353e36e2
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-05-15 17:20:06 +0200

    Import modułu database

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,3 +1,4 @@
 import unittest                                                # importuję moduł unittest do tworzenia testów jednostkowych 
 import os                                                      # importuję moduł os do operacji na systemie plików
 import tempfile                                                # importuję moduł tempfile do tworzenia tymczasowych plików
+import src.database as db                                      # importuję moduł database z katalogu src, który zawiera funkcje do zarządzania bazą danych
