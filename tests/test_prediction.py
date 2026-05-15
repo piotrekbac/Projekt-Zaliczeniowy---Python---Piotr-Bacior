f617c6c736b7d1db728aff063fc3525314439e18
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-05-15 17:30:35 +0200

    Import modułu datetime

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -1,2 +1,3 @@
 import unittest                                            # importuję moduł unittest do tworzenia testów jednostkowych 
 import pandas as pd                                        # importuję moduł pandas do manipulacji danymi w formie DataFrame
+from datetime import datetime, timedelta                   # importuję moduły datetime i timedelta do operacji na datach i czasie
