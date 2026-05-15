c0a507a5f6225223a338fd1a3764192f26e7bf72
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-05-15 17:19:43 +0200

    Import modułu tempfile

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,2 +1,3 @@
 import unittest                                                # importuję moduł unittest do tworzenia testów jednostkowych 
 import os                                                      # importuję moduł os do operacji na systemie plików
+import tempfile                                                # importuję moduł tempfile do tworzenia tymczasowych plików
