13c7121d9b9013d851b109b67aae7379ca688071
piotrekbac <piotr.bacior@microsoft.wsei.edu.pl>
2026-05-16 22:51:16 +0200

    Inicjalizacja klasy testowej - TestPrediction

--- tests/test_prediction.py
+++ tests/test_prediction.py
@@ -2,3 +2,6 @@ import unittest                                            # importuję moduł u
 import pandas as pd                                        # importuję moduł pandas do manipulacji danymi w formie DataFrame
 from datetime import datetime, timedelta                   # importuję moduły datetime i timedelta do operacji na datach i czasie
 from src.prediction import predict_goal_from_sql           # importuję funkcję predict_goal_from_sql z modułu prediction, która przewiduje cel na podstawie danych z bazy danych
+
+
+class TestPrediction(unittest.TestCase) :                  # definiuję klasę TestPrediction, która dziedziczy po unittest.TestCase, co pozwala na tworzenie testów jednostkowych
